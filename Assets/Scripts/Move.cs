20f04eb070988d429d70188fa2a20b0c872d8ea3
jpsiyu <jpsiyu@163.com>
2017-03-03 12:15:40 +0800

    Add project

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero,Vector3.back, 10f);
+     }
+}
